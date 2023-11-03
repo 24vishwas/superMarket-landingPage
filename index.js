4050cb4621febdae6c20fa0fc6df1dc72ab8b109
Vishwas JS <vishwasjs2806@gmail.com>
2023-11-03 23:15:45 +0530

    small adjustment

---
+++ index.js
@@ -0,0 +1,99 @@
+function toggleFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "nav-container") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-container";
+  }
+}
+
+// let slideIndex = 0;
+
+// function prev(){
+//     slideIndex-- 
+//     // showSlides()
+// }
+
+// function next(){
+//     slideIndex++
+//     // showSlides()
+// }
+
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 3000); // Change image every 2 seconds
+
+
+// }
+
+var timeOut = 2000;
+var slideIndex = 0;
+var autoOn = true;
+
+autoSlides();
+
+function autoSlides() {
+    timeOut = timeOut - 20;
+
+    if (autoOn == true && timeOut < 0) {
+        showSlides();
+    }
+    setTimeout(autoSlides, 20);
+}
+
+function prevSlide() {
+
+    timeOut = 2000;
+
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex--;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    if (slideIndex == 0) {
+        slideIndex = 3
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+
+    timeOut = 2000;
+
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
